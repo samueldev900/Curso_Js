842da963f5d44fcb25e7ea61e2fb742e45697a53
samueldev900 <samueloliveira900@gmail.com>
2023-09-28 20:33:04 -0300

    Update

--- Aula12_ex02/aula12-ex02.js
+++ Aula12_ex02/aula12-ex02.js
@@ -1,63 +1,65 @@
 function verificou(){
-            
-            year = document.getElementById('wow')
-            n = Number(year.value)
-            n = 2023 - n;
-            resp = document.getElementById('res')
-            radios = document.getElementsByName('grupo')
-            var valorSelecionado = "";
 
-            circle = document.getElementById('circle')
-            
-            for (var i = 0; i < radios.length; i++) {
-                if (radios[i].checked) {
-                    valorSelecionado = radios[i].value;
-                    break; // Se um botão de rádio estiver selecionado, saia do loop.
-            }
-            }
-            
+    var valorSelecionado = "";
+    const data = new Date();
+    const ano = data.getFullYear();
+    anoNasc = document.getElementById('ano1')
+    resp = document.getElementById('res')
+    radios = document.getElementsByName('grupo')
+    circle = document.getElementById('circle')
+    n = Number(anoNasc.value)
+    
 
-            if(valorSelecionado == "masculino"){
-                
-                resp.innerHTML = `Detectamos um homem com ${n} anos.`
-                if(n <= 18){
-                    circle.style.backgroundImage = 'url(imagens/menino_adolecente.png)'
-                }
-                else if (n < 45){
-                    circle.style.backgroundImage = 'url(imagens/homem_adulto.png)'
-                    
-                    
-                }
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            valorSelecionado = radios[i].value;
+            break; // Se um botão de rádio estiver selecionado, saia do loop.
+    }
+    }
 
-                else if(n > 45){
+    if(n > ano || valorSelecionado == "" || n == 0){
 
-                    circle.style.backgroundImage = 'url(imagens/idoso-att.png)'
-                    
-                    
-                }
-            }
-            else{
+        window.alert('[Erro] Confira os dados e tente novamente!!')
+    }
+    
+else{
+    if(valorSelecionado == "masculino"){
+        
+        resp.innerHTML = `Detectamos um Homem com ${ano - n} anos.`
+        if(ano - n <= 18){
 
-                resp.innerHTML = `Detectamos uma mulher com ${n} anos.`
-                if(n <= 18){
-                    circle.style.backgroundImage = 'url(imagens/menina_adolecente.png)'
-                }
-                else if (n < 45){
-                    circle.style.backgroundImage = 'url(imagens/mulher_adulta.png)'
-                    
-                    
-                }
-
-                else if(n > 45){
-
-                    circle.style.backgroundImage = 'url(imagens/mulher-idosa.png)'
-                    
-                    
-                }
-                
-
-            }
-
-
-
+            circle.style.backgroundImage = 'url(imagens/menino_adolecente.png)'
         }
+        else if (ano - n < 45){
+
+            circle.style.backgroundImage = 'url(imagens/homem_adulto.png)'
+        }
+
+        else if(ano - n > 45){
+
+            circle.style.backgroundImage = 'url(imagens/idoso-att.png)'
+        }
+    }
+    else if(valorSelecionado == "feminino"){
+
+        resp.innerHTML = `Detectamos uma Mulher com ${ano - n} anos.`
+
+        if(ano - n <= 18){
+            circle.style.backgroundImage = 'url(imagens/menina_adolecente.png)'
+        }
+        else if (ano - n < 45){
+
+            circle.style.backgroundImage = 'url(imagens/mulher_adulta.png)'
+        }
+
+        else if(ano - n > 45){
+
+            circle.style.backgroundImage = 'url(imagens/mulher-idosa.png)'
+            
+        }
+        
+    }
+
+}
+
+}
